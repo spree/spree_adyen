1121170c0dc671b1446b7ac8c8646bf0e86a9f73
Szymon Chorobski <szymon.chorobski@getvendo.com>
2025-08-28 12:26:19 +0200

    Auto configure (#22)
    * WIP auto configure
    
    * add callback to configure credentials
    
    * move configure to before save
    
    * VCR fixes
    
    * CR fixes 2
    
    * CR fixes 3
    
    * CR fixes 4
    
    * vcr fix
    
    * update cassettes
    
    * add sample hmac signature
    
    * autoconfigure: allowed origin fix
    
    fix vcrs
    
    expiry and remove creds
    
    * rm hardhoded host
    
    * test webhook
    
    * add webhook payload spec
    
    * CR fix
    
    * CR fixes
    
    * fix dirty method
    
    * change callback

---
+++ app/models/spree_adyen/custom_domain_decorator.rb
@@ -0,0 +1,15 @@
+module SpreeAdyen
+  module CustomDomainDecorator
+    def self.prepended(base)
+      base.after_commit :add_adyen_allowed_origin
+    end
+
+    def add_adyen_allowed_origin
+      return if store.adyen_gateway.blank?
+
+      SpreeAdyen::AddAllowedOriginJob.perform_later(id, store.adyen_gateway.id, 'custom_domain')
+    end
+  end
+end
+
+Spree::CustomDomain.prepend(SpreeAdyen::CustomDomainDecorator)
--- app/presenters/spree_adyen/application_info_presenter.rb
+++ app/presenters/spree_adyen/application_info_presenter.rb
@@ -1,3 +1,5 @@
+# see: https://docs.adyen.com/partners/application-information/?tab=partner-built_0_1#application-information-fields
+# some endpoints used in SpreeAdyen does not support applicationInfo (e.g. creating webhook)
 module SpreeAdyen
   class ApplicationInfoPresenter
     def to_h
