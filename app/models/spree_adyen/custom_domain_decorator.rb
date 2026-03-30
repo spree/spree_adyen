16ccab75494f12e1e4477265d867b70cba4befb5
Damian Legawiec <damian@getvendo.com>
2026-03-30 17:31:37 +0200

    fix

--- app/models/spree_adyen/custom_domain_decorator.rb
+++ app/models/spree_adyen/custom_domain_decorator.rb
@@ -12,4 +12,6 @@ module SpreeAdyen
   end
 end
 
-Spree::CustomDomain.prepend(SpreeAdyen::CustomDomainDecorator) if defined?(Spree::CustomDomain)
+if defined?(Spree::CustomDomain)
+  Spree::CustomDomain.prepend(SpreeAdyen::CustomDomainDecorator)
+end
